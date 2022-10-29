4a4eed0cbe0d16e0c06c87429b0864cffba1b390
C. Ru4$ <sp4wnx.dev@gmail.com>
2022-10-29 14:30:03 +0100

    creating smoke tests for videos

--- tests/Unit/Smoke/VideosHttpStatusTest.php
+++ tests/Unit/Smoke/VideosHttpStatusTest.php
@@ -8,8 +8,58 @@ class VideosHttpStatusTest extends ApiTestCase
 {
     public function testListAllVideosReturns200Test(): void
     {
-        $response = static::createClient()->request('GET', '/api/videos');
+        static::createClient()->request('GET', '/api/videos');
 
         self::assertResponseStatusCodeSame(200);
+    }
+
+    public function testCreateVideoShouldReturn201(): void
+    {
+        $this->createVideo();
+
+        self::assertResponseStatusCodeSame(201);
+    }
+
+    public function testUpdateVideoShouldReturn200(): void
+    {
+        $video = $this->createVideo();
+
+        static::createClient([], [
+            'headers' => ['accept' => 'application/json'],
+        ])->request('PUT', '/api/videos/' . $video->id, ['json' =>
+            [
+                'title' => 'First Video Test Updated',
+                'description' => 'Description for first Video Test ever created! Updated',
+                'url' => 'http://youtube.com/updated',
+            ]
+        ]);
+
+        self::assertResponseStatusCodeSame(200);
+    }
+
+    public function testDeleteVideoShouldReturn204(): void
+    {
+        $video = $this->createVideo();
+
+        static::createClient([], [
+            'headers' => ['accept' => 'application/json'],
+        ])->request('DELETE', '/api/videos/' . $video->id);
+
+        self::assertResponseStatusCodeSame(204);
+    }
+
+    private function createVideo()
+    {
+        $response = static::createClient([], [
+            'headers' => ['accept' => 'application/json'],
+        ])->request('POST', '/api/videos', ['json' =>
+            [
+                'title' => 'First Video Test',
+                'description' => 'Description for first Video Test ever created!',
+                'url' => 'http://youtube.com',
+            ]
+        ]);
+
+        return json_decode($response->getContent());
     }
 }
